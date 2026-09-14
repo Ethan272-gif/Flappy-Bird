9ac314bafc5a02577335942f90cc462d51a04684
Ethan272-gif <eschwab5@student.cscc.edu>
2026-09-14 16:15:00 -0400

    Point and game start and reset works with cheats

--- assets/Scripts/GameCtrl.ts
+++ assets/Scripts/GameCtrl.ts
@@ -1,7 +1,8 @@
-import { _decorator, CCInteger, Component, Node } from 'cc';
+import { _decorator, CCInteger, Component, EventKeyboard, input, Input, Node, KeyCode, director } from 'cc';
 const { ccclass, property } = _decorator;
 
 import { Ground } from './Ground';
+import { Results } from './Results';
 
 @ccclass('GameCtrl')
 export class GameCtrl extends Component {
@@ -14,6 +15,12 @@ export class GameCtrl extends Component {
     public ground:Ground
 
     @property({
+        type:Results,
+        tooltip: 'results go here'
+    })
+    public result:Results
+
+    @property({
         type:CCInteger
     })
     public speed:number=300;
@@ -24,15 +31,49 @@ export class GameCtrl extends Component {
     public pipeSpeed:number=200;
 
     onLoad(){
-
+        this.initListener();
+        this.result.resetScore();
+        director.pause();
     }
 
     initListener(){
 
+        input.on(Input.EventType.KEY_DOWN,this.onKeyDown,this);
+
+    }
+
+    // Remove once game is done and tested
+    onKeyDown(event:EventKeyboard){
+        switch(event.keyCode){
+            case KeyCode.KEY_A:
+                this.gameOver();
+            break;
+            case KeyCode.KEY_P:
+                this.result.addScore();
+            break;
+            case KeyCode.KEY_Q:
+                this.resetGame();
+        }
+
+
     }
 
     startGame(){
+        this.result.hideResults();
+        director.resume();
         
+    }
+
+    gameOver(){
+
+        this.result.showResults();
+        director.pause();
+
+    }
+    resetGame(){
+        this.result.resetScore();
+
+        this.startGame();
     }
 
 }
